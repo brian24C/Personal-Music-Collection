c6910d68993ee2a0d10bafefba1fb8ef11fe5c5c
brian24C <brianjosuecastro@hotmail.com>
2023-05-09 10:33:24 -0500

    update GridItem lg:1

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -8,14 +8,14 @@ export default function RootLayout() {
     <Grid templateColumns="repeat(6,1fr)" bg="gray.50">
       <GridItem
         as="aside"
-        colSpan={{ base: 6, lg: 2, xl: 1 }}
+        colSpan={{ base: 6, lg: 1, xl: 1 }}
         bg="brand.500"
         minHeight={{ lg: "100vh" }}
         p={{ base: "20px", lg: "30px" }}
       >
         <Sidebar />
       </GridItem>
-      <GridItem as="main" p="40px" colSpan={{ base: 6, lg: 4, xl: 5 }}>
+      <GridItem as="main" p="40px" colSpan={{ base: 6, lg: 5, xl: 5 }}>
         <Navbar />
         <Outlet />
       </GridItem>
