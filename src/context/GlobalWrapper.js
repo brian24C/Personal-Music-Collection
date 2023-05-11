4354d393ae80c01f615cbd8c926897b3caca5115
brian24C <brianjosuecastro@hotmail.com>
2023-05-10 22:18:57 -0500

    add updateSong to globalwrapper

--- src/context/GlobalWrapper.js
+++ src/context/GlobalWrapper.js
@@ -8,6 +8,9 @@ export default function Wrapper({ children }) {
   const [song, setSong] = useState({});
   const [errors, setErrors] = useState({});
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [songsFilter, setSongsFilter] = useState([]);
+
   const toast = useToast();
   const getSongs = () => {
     apiClient
@@ -20,15 +23,18 @@ export default function Wrapper({ children }) {
       });
   };
 
-  const searchSong = (id) => {
-    apiClient
-      .post(`/song/${id}`)
-      .then((res) => {
-        setUsers(res.data);
-      })
-      .catch((err) => {
-        console.log(err.reponse.data);
+  const Search = (query) => {
+    if (query === "") {
+      setSongsFilter(users);
+    } else {
+      const resultSearch = songs.filter((song) => {
+        if (song.name.toString().toLowerCase().incudes(query.toLowerCase())) {
+          return song;
+        }
       });
+
+      setSongsFilter(resultSearch);
+    }
   };
 
   const deleteSong = (id) => {
@@ -62,6 +68,39 @@ export default function Wrapper({ children }) {
         setErrors({});
         setForm({});
         onClose();
+      })
+      .catch((err) => {
+        setErrors(err.response.data.error);
+      });
+  };
+
+  const FindOne = (id) => {
+    apiClient
+      .get(`/song/${id}`)
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log(err.reponse.data);
+      });
+  };
+
+  const updateSong = (form, setForm, id) => {
+    axios
+      .put(`/song/${id}`, form)
+      .then((res) => {
+        toast({
+          title: "User Updated",
+          status: "success",
+          duration: 4000,
+          isClosable: true,
+        });
+        setErrors({});
+        setForm({});
+        onClose();
+        getSongs();
+        //Probar esto luego:
+        //setSongs(songs.map((song) => song.id===id ? res.data : song))
       })
       .catch((err) => {
         setErrors(err.response.data.error);
