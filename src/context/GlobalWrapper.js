3119bf752bd82b77ecd9d5bc1d79c976b08315c7
brian24C <brianjosuecastro@hotmail.com>
2023-05-10 21:23:28 -0500

    add servies api-clienr

---
+++ src/context/GlobalWrapper.js
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+import { useDisclosure } from "@chakra-ui/react";
+import apiClient from "../services/api-client";
+export const GlobalContext = createContext();
+
+export default function Wrapper({ children }) {
+  const [songs, setSongs] = useState([]);
+  const [user, setUser] = useState({});
+  const [errors, setErrors] = useState({});
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const fetchSongs = () => {
+    apiClient
+      .get("/api/users")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log(err.reponse.data);
+      });
+  };
+}
