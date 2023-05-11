9385d5e6ff212161f8dfa980665ee330262d3920
brian24C <brianjosuecastro@hotmail.com>
2023-05-10 21:33:22 -0500

    add function deleteSong and add toast

--- src/context/GlobalWrapper.js
+++ src/context/GlobalWrapper.js
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { useDisclosure } from "@chakra-ui/react";
+import { useDisclosure, useToast } from "@chakra-ui/react";
 import apiClient from "../services/api-client";
 export const GlobalContext = createContext();
 
@@ -8,7 +8,7 @@ export default function Wrapper({ children }) {
   const [song, setSong] = useState({});
   const [errors, setErrors] = useState({});
   const { isOpen, onOpen, onClose } = useDisclosure();
-
+  const toast = useToast();
   const fetchSongs = () => {
     apiClient
       .get("/song")
@@ -21,10 +21,27 @@ export default function Wrapper({ children }) {
   };
 
   const searchSong = (id) => {
-    axios
+    apiClient
       .post(`/song/${id}`)
       .then((res) => {
         setUsers(res.data);
+      })
+      .catch((err) => {
+        console.log(err.reponse.data);
+      });
+  };
+
+  const deleteSong = (id) => {
+    apiClient
+      .delete(`/song/${id}`)
+      .then((res) => {
+        setUsers(songs.filter((u) => u.id != id));
+        toast({
+          title: "Song Deleted",
+          status: "success",
+          duration: 4000,
+          isClosable: true,
+        });
       })
       .catch((err) => {
         console.log(err.reponse.data);
