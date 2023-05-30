0e436cbec1d53d51a697e1f7dedbdc8f0c01fbed
brian24C <brianjosuecastro@hotmail.com>
2023-05-30 12:26:35 -0500

    add useSongEdit with react-query

--- src/components/editSongs/DrawerEditSong.jsx
+++ src/components/editSongs/DrawerEditSong.jsx
@@ -15,19 +15,21 @@ import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../../context/GlobalWrapper";
 import InputsGroup from "./InputsGroup";
 import DrawerGeneral from "../DrawerGeneral";
+import useSongEdit from "../../hooks/useSongEdit";
 
-export default function DrawerExample({ song }) {
+export default function DrawerExample({ song, idPlaylist }) {
   //   const { isOpen, onClose, song, addSongToPlaylist, updateSong } =
   //     useContext(GlobalContext);
 
   const { isOpen, onOpen, onClose } = useDisclosure();
-
+  const editSong = useSongEdit(onClose, idPlaylist);
   return (
     <DrawerGeneral
       name="Create / Update user"
       data={song}
       isOpen={isOpen}
       onClose={onClose}
+      onClick={(form) => editSong.mutate(form)}
     />
   );
 }
---
+++ src/hooks/useSongEdit.js
@@ -0,0 +1,44 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import React from "react";
+import apiClient from "../services/api-client";
+import { useToast } from "@chakra-ui/react";
+
+const useSongEdit = (close, idPlaylist) => {
+  const queryClient = useQueryClient();
+  const toast = useToast();
+  return useMutation({
+    mutationFn: (data) => {
+      return apiClient
+        .put(`/song/${data.id}`, {
+          ...data,
+        })
+        .then(({ data }) => data.dataTotal);
+    },
+    onMutate: (data) => {
+      const previousSongs = queryClient.getQueryData(["songs", idPlaylist]);
+      queryClient.setQueryData(["songs", idPlaylist], (songs) =>
+        songs.map((song) => (song.id === data.id ? data : p))
+      );
+
+      toast({
+        title: "Song update successfully",
+        status: "success",
+        duration: 4000,
+        isClosable: true,
+      });
+
+      close();
+
+      return { previousSongs };
+    },
+    onSuccess: (savePlaylist, newPlaylist) => {},
+
+    onError: (error, newPlaylist, context) => {
+      if (!context) return;
+
+      queryClient.setQueryData(["songs", idPlaylist], context.previousSongs);
+    },
+  });
+};
+
+export default useSongEdit;
